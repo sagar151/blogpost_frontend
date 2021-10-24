a12583cea145f19b1a59161c33ad185ec8e203ae
sagar gajera <46403954+sagar151@users.noreply.github.com>
2021-10-24 20:09:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddBlogForm  from "./Component/AddBlogForm"
+import BlogDetails from "./Component/BlogDetails"
+import ManageBlog from "./Component/ManageBlog";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from "./Component/Home";
+import NavBar from "./Component/NavBar";
+import {  Route, Switch } from "react-router";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <NavBar/>
+
+          <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route  path="/details/:post_id" component={BlogDetails}/>
+              <Route  path="/add/:post_id" component={ManageBlog}/>
+              <Route  path="/add" component={ManageBlog}/>
+          </Switch>
+
+
+      </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-
+import store from "./Store/store";
+import {Provider} from "react-redux"
 ReactDOM.render(
   <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
+  </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
